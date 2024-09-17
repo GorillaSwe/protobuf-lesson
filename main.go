fa643af1f08bf55761234b118e1fccc5e396d470
junk237 <reds7.buddy42rich@gmail.com>
2024-09-17 21:46:47 +0900

    add: Protocol Buffersを操作

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"protobuf-lesson/pb"
+
+	"github.com/golang/protobuf/jsonpb"
+)
+
+func main() {
+	employee := &pb.Employee {
+		Id: 1,
+		Name: "Suzuki",
+		Email: "test@example.com",
+		Occupation: pb.Occupation_DESIGNER,
+		PhoneNumber: []string{"080-1234-5678", "090-1234-5678"},
+		Project: map[string]*pb.Company_Project{"ProjectX": &pb.Company_Project{}},
+		Profile: &pb.Employee_Text{
+			Text: "My name is Suzuki",
+		},
+		Birthday: &pb.Date{
+			Year: 2000,
+			Month: 2,
+			Day: 1,
+		},
+	}
+
+	m := jsonpb.Marshaler{}
+	out, err := m.MarshalToString(employee)
+	if err != nil {
+		log.Fatalln("Can't marshal to json", err)
+	}
+	fmt.Println(out)
+
+	readEmployee := &pb.Employee{}
+	if err := jsonpb.UnmarshalString(out, readEmployee); err != nil {
+		log.Fatalln("Can't unmarshal", err)
+	}
+
+	fmt.Println(readEmployee)
+}
